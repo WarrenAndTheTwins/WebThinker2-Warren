c95750451f3e3255d46c9d602cc9d71ea821d72f
WarrenAndTheTwins <warrenasks@gmail.com>
2025-10-24 02:21:26 +0000

    Oct 24, 2025, 2:21 AM

--- testquestion1.js
+++ testquestion1.js
@@ -2,13 +2,18 @@ let bird
 let background, birdImg;
 
 function preload(){
-    
+    loadImage('bird.png');
+    loadImage('background.png');
+    }
     // write code to preload bird.png and background.png
     // refer to assets folder
-}
 
 function setup(){
     new Canvas(400, 600);
+    bird = new Sprite;
+    bird.x = 200;
+    bird.y = 300;
+
     // write code to create sprite and load bird image on sprite
 }
 
