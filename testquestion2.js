6bae91f33fc6a65ab7d2df563075b6ca859bd6fc
WarrenAndTheTwins <warrenasks@gmail.com>
2025-10-24 02:36:30 +0000

    Oct 24, 2025, 2:36 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,17 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+
+    // code to create a new square sprite
+}
+
+function draw(){
+    background("grey");
+}
+
+
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
